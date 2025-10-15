2fb4c0daf86a991dd6e3ca2c1494639f4b03de98
josh098741 <morongemokaya@gmail.com>
2025-10-16 00:55:05 +0300

    Additional commit

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,26 +1,55 @@
 import React,{useState} from 'react'
 import { Search } from 'lucide-react'
+import axios from 'axios'
 
 function Body(){
 
-    const [city, setCity] = useState(null)
-    const [weather, setWeather] = useState([])
-    const [error, setError] = useState(false)
+    const [city, setCity] = useState('')
+    const [weather, setWeather] = useState(null)
+    const [error, setError] = useState('')
 
     const API_KEY = '525ed507c42449105511024dc4380e35'
+
+    const getWeather = async (event) => {
+        event.preventDefault();
+        try{
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+            setWeather(response.data)
+            setError('')
+            setCity('')
+        }catch(error){
+            console.log(error)
+            setError('City Not Found.Try Again')
+            setWeather(null)
+            setCity('')
+        }
+    }
 
     return(
         <div className="w-full h-[700px]">
             <div className="border h-[600px] m-5 rounded">
                 <div>
                     <h1 className="text-center text-xl">Enter The City</h1>
-                    <form className="border m-5 h-10 rounded flex items-center justify-between p-5">
+                    <form onSubmit={getWeather} className="border m-5 h-10 rounded flex items-center justify-between p-3">
                         <div>
-                            <Search />
+                            <Search className="cursor-not-allowed"/>
                         </div>
-                        <input type="text" className="border text-xl outline-none h-10 w-[550px]" />
+                        <input value={city} onChange={(event) => setCity(event.target.value)} type="text" className="text-xl outline-none w-[550px]" />
                         <button type="submit" className="w-60 p-2 bg-blue-400 ml-4">Submit</button>
                     </form>
+                </div>
+                <div className="border m-5 h-[400px]">
+                    {error && <div>{error}</div>}
+                    {
+                        weather && (
+                            <div className="m-5 h-[350px] flex flex-col justify-between text-center">
+                                <div className="border p-5 rounded-md">{weather.name}</div>
+                                <div className="border p-5 rounded-md">{weather.main.temp} ℃</div>
+                                <div className="border p-5 rounded-md">{weather.weather[0].description}</div>
+                                <div className="border p-5 rounded-md">Humidity: {weather.main.humidity}</div>
+                            </div>
+                        )
+                    }
                 </div>
             </div>
         </div>
